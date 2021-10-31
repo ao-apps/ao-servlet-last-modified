83a424ca7cbbfbf1f934ca7901fbab2dedea5ec4
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:55:22 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-servlet-last-modified.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-servlet-last-modified.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.servlet.lastmodified {
 	exports com.aoapps.servlet.lastmodified;
